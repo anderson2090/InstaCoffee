d973ad3aa3c97ffadecab6bf269cfd3f272c377e
usama <usamanadeem1991@gmail.com>
2018-03-23 09:38:12 +0500

    made some changes in nav drawer

--- app/src/main/java/com/sweetdeveloper/instacoffee/WelcomeActivity.java
+++ app/src/main/java/com/sweetdeveloper/instacoffee/WelcomeActivity.java
@@ -105,15 +105,14 @@ public class WelcomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_menu) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_cart) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_orders) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_change_pw) {
 
         } else if (id == R.id.nav_sign_out) {
             firebaseAuth.signOut();
