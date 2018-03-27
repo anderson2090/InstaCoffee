9b43688996936dc2d1f8fdf867808753e2db8bc5
usama <usamanadeem1991@gmail.com>
2018-03-27 21:49:41 +0500

    created  time recyclerview

--- app/src/main/java/com/sweetdeveloper/instacoffee/fragments/OrderHistoryFragment.java
+++ app/src/main/java/com/sweetdeveloper/instacoffee/fragments/OrderHistoryFragment.java
@@ -12,10 +12,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.sweetdeveloper.instacoffee.R;
+
+import java.util.ArrayList;
 
 public class OrderHistoryFragment extends Fragment {
 
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference historyReference;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     HistoryTimeRecyclerViewAdapter adapter;
@@ -24,11 +37,33 @@ public class OrderHistoryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.order_history_fragment, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
         recyclerView = view.findViewById(R.id.history_time_recycler_view);
-        layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new HistoryTimeRecyclerViewAdapter();
-        recyclerView.setAdapter(adapter);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        historyReference = firebaseDatabase.getReference("order_history").child(user.getUid());
+        historyReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                ArrayList<String> timeStamps = new ArrayList<>();
+
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String timeStamp = child.getKey();
+                    timeStamps.add(timeStamp);
+                }
+                layoutManager = new LinearLayoutManager(getActivity());
+                recyclerView.setLayoutManager(layoutManager);
+                adapter = new HistoryTimeRecyclerViewAdapter(timeStamps);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
         return view;
 
@@ -37,7 +72,12 @@ public class OrderHistoryFragment extends Fragment {
     class HistoryTimeRecyclerViewAdapter extends RecyclerView.Adapter<HistoryTimeRecyclerViewAdapter.ViewHolder> {
 
 
-        String[] times = {"One", "Two", "Three"};
+        //String[] times = {"One", "Two", "Three"};
+        ArrayList<String> timeStamps = new ArrayList<>();
+
+        HistoryTimeRecyclerViewAdapter(ArrayList<String> timeStamps) {
+            this.timeStamps = timeStamps;
+        }
 
         @NonNull
         @Override
@@ -51,12 +91,12 @@ public class OrderHistoryFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull HistoryTimeRecyclerViewAdapter.ViewHolder holder, int position) {
-            holder.timeStampTextView.setText(times[position]);
+            holder.timeStampTextView.setText(timeStamps.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return times.length;
+            return timeStamps.size();
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
