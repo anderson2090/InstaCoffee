34e386155df0a53be7a235eed885771361a64f5f
usama <usamanadeem1991@gmail.com>
2018-04-02 13:43:00 +0500

    created custom order layout

--- app/src/main/java/com/sweetdeveloper/instacoffee/WelcomeActivity.java
+++ app/src/main/java/com/sweetdeveloper/instacoffee/WelcomeActivity.java
@@ -34,6 +34,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.sweetdeveloper.instacoffee.fragments.ChangePasswordDialogFragment;
+import com.sweetdeveloper.instacoffee.fragments.CustomOrderFragment;
 import com.sweetdeveloper.instacoffee.fragments.FavouritesFragment;
 import com.sweetdeveloper.instacoffee.fragments.NewsFragment;
 import com.sweetdeveloper.instacoffee.fragments.OrderHistoryFragment;
@@ -196,6 +197,11 @@ public class WelcomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_news) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.welcome_activity_root_layout, new NewsFragment(), "newsFragment")
+                    .commit();
+            mainMenu.setEnabled(true);
+        }else if(id==R.id.nav_custom_order){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.welcome_activity_root_layout, new CustomOrderFragment(), "customOrderFragment")
                     .commit();
             mainMenu.setEnabled(true);
         }
